bb9220f87ca7eb3f37bce0f9bc1aed7553c81500
Luis Toledo <luistoledo@tuhabi.mx>
2025-08-23 20:37:00 -0500

    feat: stages

--- src/app/main.py
+++ src/app/main.py
@@ -57,8 +57,8 @@ async def infobip_webhook(webhook_data: dict):
     
     # Get agent
     context = {"chat_id": chat_id}
-    agent = AgentsFactory.get_agent(user_type, context)
-    print(f"Using agent: {agent.__class__.__name__}")
+    # agent = AgentsFactory.get_agent(user_type, context)
+    # print(f"Using agent: {agent.__class__.__name__}")
 
     # # Process message with complete conversation context
     # agent_context = {
