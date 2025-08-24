12940502fa335ad8c4f3985e86855989df6ba1f0
Edwin Neira <edwinchivata@habi.co>
2025-08-23 20:40:06 -0500

    add: temporal integration qr and image

--- src/app/utils/openai.py
+++ src/app/utils/openai.py
@@ -4,6 +4,7 @@ OpenAI utils
 from ..config import settings
 from openai import OpenAI
 from loguru import logger
+import base64
 
 
 class OpenIA:
@@ -26,3 +27,34 @@ class OpenIA:
             except Exception as e: # pylint: disable=broad-exception-caught
                 logger.error(f"Error extracting text from audio: {str(e)}")
                 return ""
+
+    def integrate_images(self, data_property) -> str:
+        """Integrate images into a property"""
+
+        response = self.openai_client.responses.create(
+            model="gpt-4o",
+            input="Toma la imagen de la propiedad y integrala con el QR code. Respeta el contenido del QR code.",
+            tools=[{"type": "image_generation"}],
+        )
+        
+
+        image_data = [
+            output.result
+            for output in response.output
+            if output.type == "image_generation_call"
+        ]
+
+        if image_data:
+            image_base64 = image_data[0]
+            with open("cat_and_otter.png", "wb") as f:
+                f.write(base64.b64decode(image_base64))
+        image_data = [
+            output.result
+            for output in response.output
+            if output.type == "image_generation_call"
+        ]
+
+        if image_data:
+            image_base64 = image_data[0]
+            with open("cat_and_otter.png", "wb") as f:
+                f.write(base64.b64decode(image_base64))
